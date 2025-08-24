cbb292dccd6e7efcee127d4a11bdf79e15afd30f
Filip Grace <filip@filbot.com>
2025-08-24 14:43:33 -0700

    Update log.h

--- log.h
+++ log.h
@@ -10,8 +10,35 @@
 #define LOG_TAG "FD"
 #endif
 
-#define LOG_ERROR(...) do{ if (LOG_LEVEL >= 0) { Serial.printf("[E][%s] ", LOG_TAG); Serial.printf(__VA_ARGS__); Serial.println(); } }while(0)
-#define LOG_WARN(...)  do{ if (LOG_LEVEL >= 1) { Serial.printf("[W][%s] ", LOG_TAG); Serial.printf(__VA_ARGS__); Serial.println(); } }while(0)
-#define LOG_INFO(...)  do{ if (LOG_LEVEL >= 2) { Serial.printf("[I][%s] ", LOG_TAG); Serial.printf(__VA_ARGS__); Serial.println(); } }while(0)
-#define LOG_DEBUG(...) do{ if (LOG_LEVEL >= 3) { Serial.printf("[D][%s] ", LOG_TAG); Serial.printf(__VA_ARGS__); Serial.println(); } }while(0)
-
+#define LOG_ERROR(...) \
+  do { \
+    if (LOG_LEVEL >= 0) { \
+      Serial.printf("[E][%s] ", LOG_TAG); \
+      Serial.printf(__VA_ARGS__); \
+      Serial.println(); \
+    } \
+  } while (0)
+#define LOG_WARN(...) \
+  do { \
+    if (LOG_LEVEL >= 1) { \
+      Serial.printf("[W][%s] ", LOG_TAG); \
+      Serial.printf(__VA_ARGS__); \
+      Serial.println(); \
+    } \
+  } while (0)
+#define LOG_INFO(...) \
+  do { \
+    if (LOG_LEVEL >= 2) { \
+      Serial.printf("[I][%s] ", LOG_TAG); \
+      Serial.printf(__VA_ARGS__); \
+      Serial.println(); \
+    } \
+  } while (0)
+#define LOG_DEBUG(...) \
+  do { \
+    if (LOG_LEVEL >= 3) { \
+      Serial.printf("[D][%s] ", LOG_TAG); \
+      Serial.printf(__VA_ARGS__); \
+      Serial.println(); \
+    } \
+  } while (0)
